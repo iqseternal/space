e5662ca8298babdfb900760a060b96965ddc8abc
iqseternal <liniyuanqu@163.com>
2023-09-30 23:17:44 +0800

    feat: wait....

---
+++ src/renderer/src/components/RInput/RInput.vue
@@ -0,0 +1,16 @@
+<script lang="tsx">
+import { defineComponent } from 'vue';
+import { Input } from 'ant-design-vue';
+
+export default defineComponent({
+  setup(props, { slots }) {
+    return () => (
+      <Input { ...props }>{slots}</Input>
+    );
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/renderer/src/pages/index/views/login/LoginForm.vue
+++ src/renderer/src/pages/index/views/login/LoginForm.vue
@@ -1,6 +1,10 @@
 <template>
   <Form>
     <FormItem hasFeedback validateStatus="success">
+      <RInput>
+        <template #prefix><UserOutlined style="color: rgba(0, 0, 0, 0.25)" /></template>
+      </RInput>
+
       <Input>
         <template #prefix><UserOutlined style="color: rgba(0, 0, 0, 0.25)" /></template>
       </Input>
@@ -18,6 +22,9 @@ import type { Ref } from 'vue';
 import { ref } from 'vue';
 import { Space, FormItem, Modal, Form, Input, InputPassword } from 'ant-design-vue';
 import { UserOutlined, LockOutlined } from '@ant-design/icons-vue';
+
+
+import RInput from '@renderer/components/RInput/RInput.vue';
 
 </script>
 
